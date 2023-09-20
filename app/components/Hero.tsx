78af52af14e7859dde5c5c17b6ebb56f655847cd
Jihan Cahya <alwirifa611@gmail.com>
2023-09-20 17:27:15 +0700

    fix bug

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -38,8 +38,8 @@ export const Hero = () => {
             <Image alt='alwi' src='/images/logo.png' width='100' height='100' className="rounded-full" />
           </div>
           <div className="leading-tight text-center mt-6">
-            <h3 className="text-4xl font-bold">I'm Alwi Rifa</h3>
-            <h3 className="text-2xl font-bold">I'm into {" "} <span className="text-sky-500">{text}</span></h3>
+            <h3 className="text-4xl font-bold">I am Alwi Rifa</h3>
+            <h3 className="text-2xl font-bold">I am into {" "} <span className="text-sky-500">{text}</span></h3>
           </div>
           <p className="max-w-xl text-center">Proficient at troubleshooting issues related to web development, IoT, data analytics, databases, servers, and networks. Skilled in crafting aesthetically UI, user-friendly websites using multiple languages and frameworks.
           </p>
