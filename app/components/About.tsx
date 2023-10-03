2669ba82a0c7787254af45f8c76f1830c9157b5c
Jihan Cahya <alwirifa611@gmail.com>
2023-10-03 09:56:01 +0700

    change design

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -33,7 +33,7 @@ function About({ }) {
 
           <div className="flex flex-col gap-4 max-w-xl text-justify">
             <p>
-              I am a <span className="font-semibold">Software Engineer</span> based in Bandung,
+              I`m a <span className="font-semibold">Software Engineer</span> based in Bandung,
               undergraduate student of Electrical Engineering at <a href="https://www.upi.edu/" className="font-semibold text-sky-500">Indonesia
                 University of Education</a>, and my journey in web development began
               during my first year of college in 2019. I am deeply passionate
@@ -53,7 +53,7 @@ function About({ }) {
               ))}
             </div>
             <p>
-              Outside of work, I amm interested in following the developments of science. I also play a lot of video games. And make youtube videos.
+              Outside of work, I`m interested in following the developments of science. I also play a lot of video games. And make youtube videos.
             </p>
             <div>
               <a href='https://drive.google.com/file/d/1LvLhyQEJC63RXyImHTRDW37B0ScvDo-M/view?usp=drive_link' className=" rounded-md px-4 py-2 font-medium text-sm bg-sky-500  hover:bg-sky-400  dark:bg-sky-500  dark:hover:bg-sky-400 text-white cursor-pointer">
